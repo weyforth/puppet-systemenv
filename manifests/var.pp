a2d0a7474e453293c4b8bc3a6d6d3452f7579a82
Mike Farrow <contact@mikefarrow.co.uk>
2015-08-08 15:45:37 +0100

    generalizing

--- manifests/var.pp
+++ manifests/var.pp
@@ -2,21 +2,32 @@ define systemenv::var (
   $ensure = 'present',
   $varname = $name,
   $value = undef,
+  $target = undef,
+  $quotes = false
 ) {
   require systemenv
 
   validate_re($ensure, '^present|absent$')
   validate_re($varname, '^[a-zA-Z][a-zA-Z0-9_]+')
   
-  $target = $systemenv::params::default_env_settings_file
+  if $target == undef {
+    $target = $systemenv::params::default_env_settings_file
+  }
+
   validate_absolute_path($target)
 
-  ini_setting { "${varname}=${value}":
+  $quote_str = ""
+
+  if $quotes == false {
+    $quote_str = "\'" 
+  }
+
+  ini_setting { "${target} ${varname}=${value}":
     ensure => $ensure,
     path => $target,
     section => '',
     setting => $varname,
-    value => "\'${value}\'",
+    value => "${quote_str}${value}${quote_str}",
     key_val_separator => '=',
   }
 }
